4ccb256cb6a7ab2fb76d6a884a7edadb2ab6e4c6
Hein Pieterse <hp.pieterse@gmail.com>
2021-07-11 12:28:58 +0200

    Add budget navigation

--- resources/views/budget.blade.php
+++ resources/views/budget.blade.php
@@ -9,11 +9,23 @@ function getSpendAmount($spendCategory){
 $maxAmount = max(array_map(static fn($budgetCategory) => 
     max(array_map('getSpendAmount',$budgetCategory->spendCategories)), $budgetCategories));
 
+$previousMonth = clone $monthStart;
+$previousMonth->sub(new DateInterval('P1M'));
+$nextMonth = clone $monthStart;
+$nextMonth->add(new DateInterval('P1M')); 
 @endphp
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <h2>{{ $monthStart->format('F Y') }}</h2>
+            <div class='d-flex align-items-center'> 
+                <a href="{{ route('budget.show', ['date' => $previousMonth->format('Y-m-1')]) }}">
+                    <x-icon-caret-left-solid class="icon" />
+                </a>
+                <h2 class="flex-fill text-center">{{ $monthStart->format('F Y') }}</h2> 
+                <a href="{{ route('budget.show', ['date' => $nextMonth->format('Y-m-1')]) }}">
+                    <x-icon-caret-right-solid class="icon" />
+                </a>
+            </div>
             @foreach ($budgetCategories as $budgetCategory)
                 <div class="card mt-4">
                     <div class="card-header d-flex">
@@ -31,7 +43,7 @@ $maxAmount = max(array_map(static fn($budgetCategory) =>
                                     <div 
                                         class="progress-bar bg-info"
                                         role="progressbar"
-                                        style="width: {{$maxAmount === 0 ? 0 : $spendCategory->amount / $maxAmount  * 100}}%"
+                                        style="width: {{$maxAmount == 0 ? 0 : $spendCategory->amount / $maxAmount  * 100}}%"
                                         aria-valuenow="{{$spendCategory->amount}}"
                                         aria-valuemin="0"
                                         aria-valuemax="{{$maxAmount}}"></div>
